91f2e724db0fbc6cbf24a19cc68862f128ddc864
Bryan Loh <bryan@end-game.com>
2018-09-30 20:23:38 +1300

    Loop playlist

--- src/assets/js/player.js
+++ src/assets/js/player.js
@@ -59,42 +59,7 @@ player.playlist([
     name: 'Slider Cells',
   },
 ]);
+
 player.playlist.autoadvance(0);
+player.playlist.repeat(true);
 player.playlistUi();
-// player.playlist([{
-//   sources: [{
-//     src: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
-//     type: 'video/mp4'
-//   }],
-//   poster: 'http://media.w3.org/2010/05/sintel/poster.png'
-// }, {
-//   sources: [{
-//     src: 'http://media.w3.org/2010/05/bunny/trailer.mp4',
-//     type: 'video/mp4'
-//   }],
-//   poster: 'http://media.w3.org/2010/05/bunny/poster.png'
-// }, {
-//   sources: [{
-//     src: 'http://vjs.zencdn.net/v/oceans.mp4',
-//     type: 'video/mp4'
-//   }],
-//   poster: 'http://www.videojs.com/img/poster.jpg'
-// }, {
-//   sources: [{
-//     src: 'http://media.w3.org/2010/05/bunny/movie.mp4',
-//     type: 'video/mp4'
-//   }],
-//   poster: 'http://media.w3.org/2010/05/bunny/poster.png'
-// }, {
-//   sources: [{
-//     src: 'http://media.w3.org/2010/05/video/movie_300.mp4',
-//     type: 'video/mp4'
-//   }],
-//   poster: 'http://media.w3.org/2010/05/video/poster.png'
-// }]);
-
-
-
-
-
-// .playlistUi();
